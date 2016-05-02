13fb75bf2c433f4499e3ce6f3cf91fc8924f5db5
lululala <et3216@gmail.com>
2016-05-02 09:22:49 +0800

    fix youtube /profile/welcome select controller

--- app/helpers/teacher_profiles_helper.rb
+++ app/helpers/teacher_profiles_helper.rb
@@ -1,7 +1,7 @@
 module TeacherProfilesHelper
   def youtube_website
-    if current_user.teacher.youtube
-      current_user.teacher.youtube.gsub("watch?v=","embed/")+"?playlist=&version=3&controls=0&fs=0&rel=0&showinfo=0&iv_load_policy=3&theme=light"
+    unless current_user.teacher.youtube.empty?
+      current_user.teacher.youtube.gsub("watch?v=","embed/")
     else
       "https://www.youtube.com/embed/z3XVg9wRVmk"
     end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,19 +1,27 @@
 class Teacher < ActiveRecord::Base
   belongs_to :user
-  has_many :languages , :through => :teacher_languageships
+  has_many :languages, through: :teacher_languageships
   has_many :teacher_languageships
   has_many :experiences
   has_many :certificates
   has_many :educations
-  accepts_nested_attributes_for :teacher_languageships , allow_destroy: true
-  accepts_nested_attributes_for :languages , allow_destroy: true
-  accepts_nested_attributes_for :experiences , allow_destroy: true
-  accepts_nested_attributes_for :certificates , allow_destroy: true
-  accepts_nested_attributes_for :educations , allow_destroy: true
+  accepts_nested_attributes_for :teacher_languageships, allow_destroy: true
+  accepts_nested_attributes_for :languages, allow_destroy: true
+  accepts_nested_attributes_for :experiences, allow_destroy: true
+  accepts_nested_attributes_for :certificates, allow_destroy: true
+  accepts_nested_attributes_for :educations, allow_destroy: true
 
   def uniq_language
     array = []
-    params[:teacher][:languages_attributes].map{|x| array << x[:language]}
+    params[:teacher][:languages_attributes].map { |x| array << x[:language] }
     array.uniq
+  end
+
+  def youtube_website
+    if self.youtube.empty?
+      'https://www.youtube.com/embed/z3XVg9wRVmk'
+    else
+      self.youtube.gsub('watch?v=', 'embed/')
+    end
   end
 end
