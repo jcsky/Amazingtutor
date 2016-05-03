2c97b7ad8cb71c93ed062518eee5dbb7c20b0c9d
lululala <et3216@gmail.com>
2016-05-03 11:59:43 +0800

    fix database

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160427163056) do
+ActiveRecord::Schema.define(version: 20160502020208) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "teacher_id"
@@ -75,7 +74,6 @@ ActiveRecord::Schema.define(version: 20160427163056) do
   end
 
   create_table "languages", force: :cascade do |t|
-    t.integer  "teacher_id"
     t.string   "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -176,7 +174,6 @@ ActiveRecord::Schema.define(version: 20160427163056) do
     t.datetime "birthday"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "authority"
     t.string   "fb_uid"
     t.string   "fb_token"
     t.text     "fb_raw_data"
@@ -184,6 +181,7 @@ ActiveRecord::Schema.define(version: 20160427163056) do
     t.string   "google_token"
     t.text     "google_raw_data"
     t.string   "locale"
+    t.string   "authority"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
