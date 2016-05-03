d6fec239b2e0f5aa06b8d6f4c96edb8d51d735d4
ShihYi <brvast@gmail.com>
2016-05-03 20:15:59 +0800

    add user's default image by paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   has_many :evalutions
   has_many :user_available_sections
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "100x100>", thumb: "50x50>" }, default_url: "baby.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def display_name
@@ -91,4 +91,5 @@ class User < ActiveRecord::Base
     user.save!
     return user
   end
+
 end
