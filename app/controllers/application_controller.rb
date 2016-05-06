74525c134b95a3569e089122d80f5e73ed06a2c6
lululala <et3216@gmail.com>
2016-05-06 21:10:16 +0800

    fix configure_permitted_parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :store_location
-  before_action :set_time_zone, :if => :user_signed_in?
+  before_action :set_time_zone, if: :user_signed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def set_time_zone
-    if !current_user.time_zone.nil?
-      Time.zone = current_user.time_zone
-    end
+    Time.zone = current_user.time_zone unless current_user.time_zone.nil?
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :nick)
+    end
+        end
 
   def store_location
     # store last url - this is needed for post-login redirect to whatever the user last visited.
