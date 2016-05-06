feb3e8363fa2ab682119368c9c108671c370cea8
LiuRoger <liuroger@LiuRogerde-MacBook-Pro.local>
2016-05-06 22:00:30 +0800

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160505185649) do
-
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "teacher_id"
@@ -189,7 +187,6 @@ ActiveRecord::Schema.define(version: 20160505185649) do
     t.datetime "birthday"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "authority"
     t.string   "fb_uid"
     t.string   "fb_token"
     t.text     "fb_raw_data"
@@ -197,12 +194,13 @@ ActiveRecord::Schema.define(version: 20160505185649) do
     t.string   "google_token"
     t.text     "google_raw_data"
     t.string   "locale"
+    t.string   "authority"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.boolean  "admin"
     t.string   "authentication_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
