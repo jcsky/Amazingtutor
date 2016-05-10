ca8e33ec282cc9026b0a9954b1ca2ec0b368e155
lululala <et3216@gmail.com>
2016-05-10 19:40:46 +0800

    modify login button

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,15 @@
 class WelcomeController < ApplicationController
   def index
     @teacher =Teacher.all
-     render layout: 'welcome' 
+     render layout: 'welcome'
+     if params[:lan_id]
+      #  @sortins = 
+     end
   end
 
   def apply_teacher
+  end
+
+  def sorting
   end
 end
