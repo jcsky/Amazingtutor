5fc4400e8222676b459acb1533ee2b8fe9e88e2e
lululala <et3216@gmail.com>
2016-05-15 14:23:38 +0800

    sorting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    @teacher =Teacher.all
-     render layout: 'welcome'
-    #  if params[:lan_id]
-    #   v = params[:lan_id]
-    #   @ = Teacher.all.language
-    #  end
+    if params[:lan_id].nil?
+      @teacher = Teacher.all
+      byebug
+    elsif params[:lan_id]
+      @lan_id = params[:lan_id]
+      @teacher = Teacher.includes(:teacher_languageships).where(teacher_languageships: { language_id: @lan_id }).order('id DESC')
+
+    end
+    render layout: 'welcome'
   end
 
   def apply_teacher
