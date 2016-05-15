0e18b75808b86961dac8cb3a3b184be7e2e63226
lululala <et3216@gmail.com>
2016-05-15 15:49:11 +0800

    fix welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:lan_id].nil?
       @teacher = Teacher.all
-      byebug
-    elsif params[:lan_id]
-      @lan_id = params[:lan_id]
-      @teacher = Teacher.includes(:teacher_languageships).where(teacher_languageships: { language_id: @lan_id }).order('id DESC')
-
-    end
-    render layout: 'welcome'
+      render layout: 'welcome'
   end
 
   def apply_teacher
