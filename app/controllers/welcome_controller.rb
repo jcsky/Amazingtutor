3dbb57e5a5b55317781a88c3810c42e48bd9f79e
lululala <et3216@gmail.com>
2016-05-15 16:20:19 +0800

    language sorting finish

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @teachers = Teacher.all
+
     if params[:language_id]
       @lan_id = params[:language_id]
-      @teachers = Teacher.includes(:teacher_languageships).where(teacher_languageships: { language_id: @lan_id })
+      @teachers = Teacher.includes(:teacher_languageships).where(:teacher_languageships=>{language_id: @lan_id })
+    else
+      @teachers = Teacher.all
     end
   end
 
