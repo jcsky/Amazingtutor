d7fd47880cbdf1782f8b9fb2d0fe84f88971c9b6
Damon Liao <damon09273@gmail.com>
2016-05-16 15:51:18 +0800

    add GA and GTM

--- db/schema.rb
+++ db/schema.rb
@@ -193,8 +193,8 @@ ActiveRecord::Schema.define(version: 20160505185649) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.boolean  "admin"
     t.string   "authentication_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
