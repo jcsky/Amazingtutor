389c0ed87c05f688233f2450186e50987deb65cf
lululala <et3216@gmail.com>
2016-05-20 17:31:14 +0800

    sorting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,21 +1,51 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:language_id] && params[:week].nil?
-    find_language_teacher
-    elsif params[:week] && params[:language_id].nil?
+  if params[:language_id] && params[:week].blank?
+      cookies[:lan]= params[:language_id]
+      cookies[:week]= nil
+      find_language_teacher
+    elsif params[:week] && params[:language_id].blank?
+      cookies[:lan]= nil
+      cookies[:week]= params[:week]
       find_week_teacher
     elsif params[:week] && params[:language_id]
+      cookies[:lan]= params[:language_id]
+      cookies[:week]= params[:week]
       find_language_teacher
       @teachera = @teachers
       find_week_teacher
       @teacherb = @teachers
       @teachers = @teachera & @teacherb
     elsif params[:price]
-      find_price_teacher
+      if cookies[:lan].blank? && cookies[:week].blank?
+        find_price_teacher
+      elsif cookies[:lan] && cookies[:week].blank?
+        find_language_teacher
+        @teachera = @teachers
+        find_price_teacher
+        @teacherb = @teachers
+        @teachers = @teachera & @teacherb
+      elsif cookies[:lan].blank? && cookies[:week]
+        find_week_teacher
+        @teachera = @teachers
+        find_price_teacher
+        @teacherb = @teachers
+        @teachers = @teachera & @teacherb
+      elsif cookies[:lan] && cookies[:week]
+        find_language_teacher
+        @teachera = @teachers
+        find_week_teacher
+        @teacherb = @teachers
+        find_price_teacher
+        @teacherc = @teachers
+        @teachers = @teachera & @teacherb & @teacherc
+      end
       respond_to do |format|
         format.js { render 'teacher' }
       end
-    else
+    elsif  params[:week].blank? && params[:language_id].blank?
+      cookies[:lan]= nil
+      cookies[:week]= nil
       @teachers = Teacher.all
     end
   end
@@ -35,12 +65,12 @@ class WelcomeController < ApplicationController
   end
 
   def find_language_teacher
-    @lan_id = params[:language_id]
+    @lan_id = cookies[:lan]
     @teachers = Teacher.includes(:teacher_languageships).where(teacher_languageships: { language_id: @lan_id }).uniq
   end
 
   def find_week_teacher
-    @week = params[:week].to_i - 1
+    @week = cookies[:week].to_i - 1
     @thedaytime = Time.now.utc.at_beginning_of_week + @week.day
     @thedaytimeend = @thedaytime + 60 * 60 * 24 - 1
     if @thedaytime == Time.now.utc.beginning_of_day
