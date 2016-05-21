8ac2b3fd81d3141d7b32fb4aa723c8a4da2d819d
lululala <et3216@gmail.com>
2016-05-21 19:35:35 +0800

    frontend fix

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 20160519080814) do
   end
 
   create_table "languages", force: :cascade do |t|
-    t.integer  "teacher_id"
     t.string   "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -184,7 +183,6 @@ ActiveRecord::Schema.define(version: 20160519080814) do
     t.datetime "birthday"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "authority"
     t.string   "fb_uid"
     t.string   "fb_token"
     t.text     "fb_raw_data"
@@ -192,12 +190,13 @@ ActiveRecord::Schema.define(version: 20160519080814) do
     t.string   "google_token"
     t.text     "google_raw_data"
     t.string   "locale"
+    t.string   "authority"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "authentication_token"
     t.boolean  "admin"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
