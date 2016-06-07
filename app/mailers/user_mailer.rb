54b03bf73d5d74d4fbd9e3f60e34f482da3210a7
ShihYi <brvast@gmail.com>
2016-06-07 11:59:28 +0800

    teacher appointment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,15 @@
 class UserMailer < ApplicationMailer
   default :from => "amazingtutor <amazingtutor@amazingtutor.com>"
 
-  def notify_teacher_new_appointment(user, teacher)
+  def notify_teacher_new_appointment(user, teacher_user)
     @user = user
-    @teacher = teacher
-    if teacher.email
-      mail(:to => teacher.alternate_email, :subject => "You have a new appointment-AmazingTutor")
+    @teacher_user = teacher_user
+    if teacher_user.alternate_email
+      mail(:to => teacher_user.alternate_email, :subject => "You have a new appointment-AmazingTutor")
     else
-      mail(:to => teacher.alternate_email, :subject => "You have a new appointment-AmazingTutor")
+      mail(:to => teacher_user.email, :subject => "You have a new appointment-AmazingTutor")
     end
   end
+
 
 end
