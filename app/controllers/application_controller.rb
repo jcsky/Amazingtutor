af8ec8a7009db54b180ad57e1025cdd047070125
lululala <et3216@gmail.com>
2016-06-12 01:10:14 +0800

    fix apply teacher bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(_resource)
     session[:previous_url] || teacherwall_path
+    byebug
   end
 
   def amazing_crypt(action,object)
